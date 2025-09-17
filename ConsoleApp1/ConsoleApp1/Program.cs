a8b9467c19f7dbe9dc6b78f62b309281427be29b
unknown <estelle1751@yandex.ru>
2025-09-17 15:52:04 +0300

    fdsf

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 5;
+            int c = a+b;
+        }
+    }
+}
